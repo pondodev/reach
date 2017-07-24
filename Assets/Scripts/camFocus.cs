0235b128e1796e1762d3c5bc4c56da4cf049a2f3
p0isonedpanda <daniel.coady@outlook.com.au>
2017-07-25 02:34:34 +1000

    Initial commit

---
+++ Assets/Scripts/camFocus.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camFocus : MonoBehaviour
+{
+    float timer = 0.0f;
+
+	// Update is called once per frame
+	void Update ()
+    {
+        timer += Time.deltaTime;
+        if (timer > 0.5f)
+        {
+            timer = 0.0f;
+            transform.localPosition = new Vector2(Random.Range(-2.0f, 2.0f), Random.Range(-2.0f, 2.0f));
+        }
+	}
+}
---
+++ Assets/Scripts/camFollow.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camFollow : MonoBehaviour
+{
+    [SerializeField]
+    GameObject player;
+    Vector3 refVel = Vector3.zero;
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.position = Vector3.SmoothDamp(transform.position, new Vector3(player.transform.position.x, player.transform.position.y, player.transform.position.z - 1), ref refVel, 0.5f, 20f, Time.deltaTime);
+	}
+}
