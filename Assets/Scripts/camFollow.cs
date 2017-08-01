6578a87e2a08ab3e8796716e347f988b5796b769
p0isonedpanda <daniel.coady@outlook.com.au>
2017-08-02 01:18:52 +1000

    Increased max camera follow speed, started level

--- Assets/Scripts/camFollow.cs
+++ Assets/Scripts/camFollow.cs
@@ -11,7 +11,7 @@ public class camFollow : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
     {
-        transform.position = Vector3.SmoothDamp(transform.position, new Vector3(player.transform.position.x, player.transform.position.y, player.transform.position.z - 1), ref refVel, 0.5f, 20f, Time.deltaTime);
+        transform.position = Vector3.SmoothDamp(transform.position, new Vector3(player.transform.position.x, player.transform.position.y, player.transform.position.z - 1), ref refVel, 0.5f, 100f, Time.deltaTime);
         transform.rotation = Quaternion.Slerp(transform.rotation, player.transform.rotation, 0.01f);
 	}
 }
