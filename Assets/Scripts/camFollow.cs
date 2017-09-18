edd8b71ad1ca7d13fdce4af3b22ec207b318ed8e
p0isonedpanda <daniel.coady@outlook.com.au>
2017-09-18 16:02:03 +1000

    Added camera delay at the start to give more room to record for showreel

--- Assets/Scripts/camFollow.cs
+++ Assets/Scripts/camFollow.cs
@@ -7,11 +7,26 @@ public class camFollow : MonoBehaviour
     [SerializeField]
     GameObject player;
     Vector3 refVel = Vector3.zero;
+    bool ready;
 	
+    void Start ()
+    {
+        StartCoroutine(Wait());
+    }
+
 	// Update is called once per frame
 	void Update ()
     {
-        transform.position = Vector3.SmoothDamp(transform.position, new Vector3(player.transform.position.x, player.transform.position.y, player.transform.position.z - 1), ref refVel, 0.5f, 100f, Time.deltaTime);
-        transform.rotation = Quaternion.Slerp(transform.rotation, player.transform.rotation, 0.01f);
+        if (ready)
+        {
+            transform.position = Vector3.SmoothDamp(transform.position, new Vector3(player.transform.position.x, player.transform.position.y, player.transform.position.z - 1), ref refVel, 0.5f, 100f, Time.deltaTime);
+            transform.rotation = Quaternion.Slerp(transform.rotation, player.transform.rotation, 0.01f);
+        }
 	}
+
+    IEnumerator Wait ()
+    {
+        yield return new WaitForSeconds(2.0f);
+        ready = true;
+    }
 }
